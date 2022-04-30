ca007931d417b0a8416e1098ec72fcc7fd9b55a2
journey_park <80277274+jihyeun-park@users.noreply.github.com>
2022-05-01 02:15:55 +0900

    EFUB_Week6_0502

---
+++ router-praactice/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Nav from "./components/Nav";
+import Transition from "./components/Transition";
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
---
+++ router-praactice/src/components/Transition.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Page from "./Page";
+import "./Transition.css";
